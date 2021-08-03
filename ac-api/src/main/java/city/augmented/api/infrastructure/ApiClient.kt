78c177b1018f4a9277d78b43bb51371183e5e976
Itanik <yan.xhrome@gmail.com>
2021-08-04 00:41:41 +0300

    Configure ApiClient, update server urls.

--- ac-api/src/main/java/city/augmented/api/infrastructure/ApiClient.kt
+++ ac-api/src/main/java/city/augmented/api/infrastructure/ApiClient.kt
@@ -5,40 +5,62 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApiClient(
-    private var baseUrl: String = ServerProperties.mainUrl,
+    private var baseUrl: ServerUrl = ServerProperties.mainUrl,
+    private val retrofitBuilder: Retrofit.Builder,
     private val serializerBuilder: Moshi.Builder = Serializer.moshiBuilder,
     private val okHttpClientBuilder: OkHttpClient.Builder? = null
 ) {
-
-    private val retrofitBuilder: Retrofit.Builder by lazy {
-        Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .addConverterFactory(MoshiConverterFactory.create(serializerBuilder.build()))
+    companion object {
+        private const val URL_PREFIX = "http://"
+        private const val URL_SSL_PREFIX = "https://"
+        private const val URL_POSTFIX = "/"
     }
 
+    private val scalarsConverterFactory: ScalarsConverterFactory by lazy {
+        ScalarsConverterFactory.create()
+    }
+    private val moshiMainFactory: MoshiConverterFactory by lazy {
+        MoshiConverterFactory.create(serializerBuilder.build())
+    }
     private val clientBuilder: OkHttpClient.Builder by lazy {
         okHttpClientBuilder ?: defaultClientBuilder
     }
-
     private val defaultClientBuilder: OkHttpClient.Builder by lazy {
         OkHttpClient().newBuilder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.MINUTES)
+            .writeTimeout(5, TimeUnit.MINUTES)
     }
 
-    init {
-        normalizeBaseUrl()
+    fun changeServerUrl(newUrl: ServerUrl) {
+        baseUrl = newUrl
     }
 
     fun <S> createService(serviceClass: Class<S>): S {
         val usedCallFactory = clientBuilder.build()
-        return retrofitBuilder.callFactory(usedCallFactory).build().create(serviceClass)
+        return retrofitBuilder
+            .baseUrl(prepareUrl(baseUrl))
+            .addConverterFactory(scalarsConverterFactory)
+            .addConverterFactory(moshiMainFactory)
+            .callFactory(usedCallFactory)
+            .build()
+            .create(serviceClass)
     }
 
-    private fun normalizeBaseUrl() {
-        if (!baseUrl.endsWith("/")) {
-            baseUrl += "/"
-        }
+    private fun prepareUrl(serverUrl: ServerUrl): String {
+        return StringBuilder().apply {
+            if (ServerProperties.serverUrls.contains(serverUrl))
+                if (serverUrl.useSSL)
+                    append(URL_SSL_PREFIX)
+                else
+                    append(URL_PREFIX)
+            else
+                append(URL_SSL_PREFIX)
+            append(serverUrl)
+            append(URL_POSTFIX)
+        }.toString()
     }
 }
--- ac-api/src/main/java/city/augmented/api/infrastructure/ServerProperties.kt
+++ ac-api/src/main/java/city/augmented/api/infrastructure/ServerProperties.kt
@@ -1,20 +1,25 @@
 package city.augmented.api.infrastructure
 
 object ServerProperties {
-    val serverUrls: List<String> = listOf(
-        "developer.augmented.city",
-        "185.162.94.228:25000",
-        "developer.mirror.augmented.city",
-        "185.162.94.228:35000",
-        "developer.reserve.augmented.city",
-        "178.249.69.197:36000",
-        "178.249.69.197:37000",
-        "developer.testing.augmented.city",
-        "185.162.94.228:15000",
-        "185.162.94.228:16000",
-        "185.162.94.228:17000"
+    val serverUrls: List<ServerUrl> = listOf(
+        ServerUrl("developer.augmented.city"),
+        ServerUrl("185.162.94.228:25000", false),
+        ServerUrl("developer.mirror.augmented.city"),
+        ServerUrl("185.162.94.228:35000", false),
+        ServerUrl("developer.reserve.augmented.city"),
+        ServerUrl("178.249.69.197:36000", false),
+        ServerUrl("178.249.69.197:37000", false),
+        ServerUrl("developer.testing.augmented.city"),
+        ServerUrl("185.162.94.228:15000", false),
+        ServerUrl("185.162.94.228:16000", false),
+        ServerUrl("185.162.94.228:17000", false)
     )
 
-    val mainUrl: String
+    val mainUrl: ServerUrl
         get() = serverUrls[0]
 }
+
+class ServerUrl(
+    val url: String,
+    val useSSL: Boolean = true
+)
