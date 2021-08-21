6859e35688f7d2471e3074fa1c5ffccf314cf3d4
Itanik <yan.xhrome@gmail.com>
2021-08-22 00:08:08 +0300

    Add comments.

--- ac-api/src/main/java/city/augmented/api/Models3dApiClient.kt
+++ ac-api/src/main/java/city/augmented/api/Models3dApiClient.kt
@@ -11,6 +11,13 @@ class Models3dApiClient(private val apiClient: ACApiClient) {
     private val models3dApi: Models3dApi
         get() = apiClient.getModels3dApi()
 
+    /**
+     * Get list of available 3D objects with link on binary resources such as glb model or unity bundle.
+     *
+     * @param platform configures the return of objects that are present for the specified platform.
+     * With param null will returns all available objects for all platforms.
+     * @return Model3dDto or ApiError
+     */
     suspend fun get3dModels(platform: PlatformType? = null): Either<ApiError, List<Model3dDto>> =
         safeInvoke { models3dApi.getModelsList(platform) }
 }
